52a980860cedcc89ceb982890837803aa596cc05
ido <idoitzhak5@gmail.com>
2022-11-06 12:26:23 +0200

    MusicApp

--- App/App.js
+++ App/App.js
@@ -1,12 +1,31 @@
 import React from "react";
-import { View,Text,StyleSheet} from 'react-native';
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const App = () => {
+
+  const doSomething = () => {
+    Alert.alert('WHAT!?','I TOLD YOU NOT TO');
+  }
+
   return(
-    <View>
-      <Text>Hello Suzan</Text>
+    <View style={myStyle.container}>
+      <TouchableOpacity style={myStyle.btn} onPress={doSomething}>
+      <Text style={myStyle.btntext}>Don't Click Me</Text>
+      </TouchableOpacity>
     </View>
   )
 }
+
+const myStyle = StyleSheet.create({
+  
+  container: {flex:1, alignItems:'center', padding:30,
+   justifyContent:'center', backgroundColor:'#DEE5E5'
+  },
+
+  btn:{width:'100%',paddingVertical:18,
+  alignItems:'center',backgroundColor:'#17B890',borderRadius:14
+},
+  btntext:{color:'#DEE5E5', fontSize:22, fontWeight:'700'},
+})
 
 export default App;
