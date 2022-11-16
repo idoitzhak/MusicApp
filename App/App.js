d3d08871223018c31c0589c85ad06fd0f43e8509
ido <idoitzhak5@gmail.com>
2022-11-16 11:23:05 +0200

    Update App.js

--- App/App.js
+++ App/App.js
@@ -1,24 +1,23 @@
 import React, {useState} from "react";
 import { Alert, StyleSheet, Text, TouchableOpacity, View,
-  TextInput, ActivityIndicator,FlatList } from 'react-native';
+  TextInput, ActivityIndicator,FlatList,Image } from 'react-native';
 
 const App = () => {
 
-  const [searchName, setSearchName] = useState('');
+  const [searchName, setSearchName] = useState('characters');
   const [isLoading, setIsLoading] = useState(false);
-  const [results,setResults] = useState({});
+  const [results,setResults] = useState([]);
 
   const doSomething = async() => {
     setIsLoading(true);
     //Alert.alert('Songs List','Ready');
-    const api = `https://itunes.apple.com/search?term=${searchName}`;
+    const api = `https://www.breakingbadapi.com/api/${searchName}`;
     const response = await fetch(api, {
       method: 'get'
     });
 
     const data = await response.json();
-    setResults(data.results);
-    console.log(results);
+    setResults(data);
     setIsLoading(false);
     
   }
@@ -56,6 +55,7 @@ const App = () => {
         <Text style={myStyle.btntext}>Search</Text>
         </TouchableOpacity>
         )
+        
       }
         </View>
       </View>
@@ -63,15 +63,23 @@ const App = () => {
       <View style={{width: '100%', height:'90%'}}>
 
         {
-          results?.results ? (
+          results ? (
             <FlatList
             data={results}
-            keyExtractor={item => item.trackId}
+            keyExtractor={item => item.char_id}
             renderItem={itemRow =>
-            <View>
-              <Text>{itemRow.item.artistName}</Text>
-            </View>}
-            />
+              <View style={myStyle.fullCont}>
+              <View style={myStyle.dataCont}>
+              <Image style={myStyle.imgContainer} source={{uri:itemRow.item.img}}/>
+              <View style={myStyle.textContainer}>
+              <Text style={myStyle.bigName}>{itemRow.item.name}</Text>
+              <Text style={myStyle.nickName}>{itemRow.item.nickname}</Text>
+              <Text>______________________________</Text>
+              <Text style={myStyle.occup}>{itemRow.item.occupation}</Text>
+             </View>
+             </View>
+             </View>
+            }/>
           ) : (
             <Text> No results</Text>
           )
@@ -83,7 +91,15 @@ const App = () => {
 }
 
 const myStyle = StyleSheet.create({
-  
+  fullCont:{padding:7},
+  dataCont:{width:'100%', height:120, flexDirection:'row',
+  backgroundColor:'white',borderRadius:10},
+  textContainer:{width:'70%', height:'100%', padding:7,},
+  bigName:{fontSize:22,fontWeight:'700', color:'#17B890'},
+  nickName:{fontSize:16,fontWeight:'700'},
+  occup:{fontSize:11,fontWeight:'650',},
+  imgContainer: {width:'35%', height:'100%',borderRadius:10},
+
   container: {flex:1, alignItems:'center', padding:20,
    justifyContent:'center', backgroundColor:'#DEE5E5'
   },
